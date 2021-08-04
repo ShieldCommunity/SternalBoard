f596fe4c1e987f0350ebbc0b0cceaa9c43851524
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-08-04 08:45:29 -0500

    Update MainCMD.java
    The general command was organized along with its permissions

--- src/main/java/com/xIsm4/plugins/commands/MainCMD.java
+++ src/main/java/com/xIsm4/plugins/commands/MainCMD.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 
 
 public class MainCMD implements CommandExecutor {
+
     private Main plugin;
 
     public MainCMD(Main plugin) {
@@ -19,42 +20,47 @@ public class MainCMD implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command comando, String label, String[] args) {
+
         if (!(sender instanceof Player)) {
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + " [X] ยกU aren't a player!");
-            return false;
-        } else {
-            Player p = (Player) sender;
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("version")) {
-                    p.sendMessage(plugin.getName() + ChatColor.WHITE + " The version of the plugin is 1.13.10");
-                    return true;
-                } else if (args[0].equalsIgnoreCase("help")) {
-                    if (p.hasPermission("sternalboard.help")) ;
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&bThis server it's runing &9&lSternal&f&lBoard"));
-                    return true;
-                } else if (args[0].equalsIgnoreCase("reload")) {
-                        //Removed some catching threads.
-                    if (p.hasPermission("sternalboard.use")) {
-                        plugin.reloadConfig();
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bThe Plugin &2[&6&lSternal&e&lBoard&2] &ahas been reloaded!"));
-                    } else {
-                        p.sendMessage(ChatColor.DARK_RED + " [X] U don't have the permission (sternalboard.use) to preform this action");
-                    }
-                    return true;
-                }else{
-                            p.sendMessage(ChatColor.RED + " The command doesn't exist!");
-                            return true;
-                        }
-                    } else {
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', " &fUse &e/sternalboard help &fto see more info about the plugin"));
-                        return true;
-                    }
-                }
-            }
+            return true;
         }
 
+        Player player = (Player) sender;
 
+        if (!(args.length > 0)) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', " &fUse &e/sternalboard help &fto see more info about the plugin"));
+            return true;
+        }
 
+        if (args[0].equalsIgnoreCase("version")) {
+            player.sendMessage(plugin.getName() + ChatColor.WHITE + " The version of the plugin is 1.13.10");
+            return true;
 
+        } else if (args[0].equalsIgnoreCase("help")) {
 
+            if (!(player.hasPermission("sternalboard.help"))) {
+                player.sendMessage(ChatColor.DARK_RED + " [X] U don't have the permission (sternalboard.help) to preform this action");
+                return true;
+            }
+
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&bThis server it's runing &9&lSternal&f&lBoard"));
+            return true;
+
+        } else if (args[0].equalsIgnoreCase("reload")) {
+
+            if (!(player.hasPermission("sternalboard.reload"))) {
+                player.sendMessage(ChatColor.DARK_RED + " [X] U don't have the permission (sternalboard.reload) to preform this action");
+                return true;
+            }
+
+            plugin.reloadConfig();
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bThe Plugin &2[&6&lSternal&e&lBoard&2] &ahas been reloaded!"));
+            return true;
+        } else {
+            player.sendMessage(ChatColor.RED + " The command doesn't exist!");
+        }
+        return true;
+    }
+}
 //Close the main thread of commands.
