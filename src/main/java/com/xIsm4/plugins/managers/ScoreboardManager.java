cc7d5e89303c378d658bb39178db2c5c9c3214c4
Ismael martinez <76608233+xIsm4@users.noreply.github.com>
2021-08-25 01:34:18 +0200

    Removed placeholder-refresh .

--- src/main/java/com/xIsm4/plugins/managers/ScoreboardManager.java
+++ src/main/java/com/xIsm4/plugins/managers/ScoreboardManager.java
@@ -18,7 +18,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 @Setter
 public class ScoreboardManager {
 
-    FileConfiguration configuration = Main.get().getConfig();
     private final Main core;
     private ConcurrentMap<UUID, SternalBoard> boards = new ConcurrentHashMap<>();
     private Object AsynchronousFileChannel;
@@ -29,10 +28,9 @@ public class ScoreboardManager {
     }
     //Update tasks > 20
     public void init() {
-        if(configuration.getBoolean("REFRESH_REALTIME.PLACEHOLDERS")) {
+
             if (core.getConfig().getInt("settings.scoreboard.update") <= 0) {
                 core.getConfig().set("settings.scoreboard.update", 20);
-            }
         }
 
         core.getServer().getScheduler().runTaskTimerAsynchronously(core, () -> {
