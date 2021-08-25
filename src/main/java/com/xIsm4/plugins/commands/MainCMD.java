b10ca3556bcdadfa5e2637bfd6fe2d02df8dc0bb
bixgamer 707 <85091403+bixgamer707@users.noreply.github.com>
2021-08-25 15:57:48 -0500

    Update MainCMD
    Spaninglish fixed and improved readability

--- src/main/java/com/xIsm4/plugins/commands/MainCMD.java
+++ src/main/java/com/xIsm4/plugins/commands/MainCMD.java
@@ -21,15 +21,12 @@ public class MainCMD implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command comando, String label, String[] args) {
-
+    public boolean onCommand(CommandSender sender, Command commands, String label, String[] args) {
         if (!(sender instanceof Player)) {
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + " [X] U aren't a player!");
-            return true;
+            return false;
         }
-
         Player player = (Player) sender;
-
         if (!(args.length > 0)) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', " &fUse &e/sternalboard help &fto see more info about the plugin"));
             return true;
@@ -40,7 +37,6 @@ public class MainCMD implements CommandExecutor {
             return true;
 
         } else if (args[0].equalsIgnoreCase("help")) {
-
             if (!(player.hasPermission("sternalboard.help"))) {
                 player.sendMessage(PlaceholderUtils.colorize("&cU don't have permissions to use this command"));
                 return true;
@@ -50,12 +46,10 @@ public class MainCMD implements CommandExecutor {
             return true;
 
         } else if (args[0].equalsIgnoreCase("reload")) {
-
             if (!(player.hasPermission("sternalboard.reload"))) {
                 player.sendMessage(PlaceholderUtils.colorize("&cU don't have permissions to use this command"));
                 return true;
             }
-
             plugin.reloadConfig();
             player.sendMessage(PlaceholderUtils.colorize("&aThe plugin has been reloaded sucesfully"));
             return true;
