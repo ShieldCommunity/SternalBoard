fa1e2636f97304011b01d0bb763eb6dd04818382
bixgamer 707 <85091403+bixgamer707@users.noreply.github.com>
2021-11-09 11:32:27 -0500

    Update Structure.java

--- src/main/java/com/xIsm4/plugins/Structure.java
+++ src/main/java/com/xIsm4/plugins/Structure.java
@@ -11,47 +11,35 @@ import lombok.Getter;
 @Getter
 public class Structure extends SetupManager {
     private static Structure instance;
-
     public static Structure getInstance() {
         return instance;
     }
-
     @Override
     public void onEnable() {
         //Set instance
         instance = this;
-
         //Load config
         loadConfig();
-
         //Load scoreboardManager (and Animated if applicable)
         loadScoreboardMgr(this);
-
         //Load listeners and commands
         getServer().getPluginManager().registerEvents(new AddBoardsListener(this), this);
         getServer().getPluginManager().registerEvents(new RemoveBoardsListener(this), this);
         commandHandler(this);
     }
-
     @Override
     public void onDisable() {
         getLogger().info("Disabling SternalBoard");
-        instance = null;
-        scoreboardManager = null;
     }
-
     public ScoreboardManager getScoreboardManager() {
         return this.scoreboardManager;
     }
-
     public AnimationManager getAnimationManager() {
         return this.animationManager;
     }
-
     public boolean isAnimationEnabled(){
         return this.animateScoreboard;
     }
-
     public YamlConfiguration getAnimConfig(){
         return this.animConfig;
     }
