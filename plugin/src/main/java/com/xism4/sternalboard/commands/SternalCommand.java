52c88da2424cc09050bd50480e46f9d9c2a65e4b
jonakls <asmot54@hotmail.com>
2022-05-01 21:37:34 -0500

    feat: added command in cli

--- plugin/src/main/java/com/xism4/sternalboard/commands/SternalCommand.java
+++ plugin/src/main/java/com/xism4/sternalboard/commands/SternalCommand.java
@@ -6,20 +6,54 @@ import com.xism4.sternalboard.utils.PlaceholderUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
-import java.util.Locale;
 
 public class SternalCommand implements CommandExecutor {
 
     private final Structure core;
+    private final FileConfiguration config;
 
     public SternalCommand(Structure plugin) {
         this.core = plugin;
+        this.config = plugin.getConfig();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command commands, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            if (args.length <= 0) {
+                sender.sendMessage(
+                        PlaceholderUtils.colorize("&eUse sternalboard help &fto see more info about the plugin")
+                );
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                core.reloadConfig();
+                core.setAnimateScoreboard(config.getBoolean("settings.animated"));
+                core.getScoreboardManager().reload();
+                if (core.isAnimationEnabled()) {
+                    core.loadAnimConfig();
+                    if (core.getAnimationManager() != null) {
+                        core.getAnimationManager().reload();
+                    } else {
+                        core.setAnimationManager(new AnimationManager());
+                    }
+                } else {
+                    if (core.getAnimationManager() != null) {
+                        core.getAnimationManager().reload();
+                    }
+                }
+                sender.sendMessage(PlaceholderUtils.colorize("&aThe plugin has been reloaded successfully"));
+                return true;
+            }
+
+            sender.sendMessage(PlaceholderUtils.colorize("&cCommand not recognised!"));
+            return true;
+        }
+
         Player player = (Player) sender;
         
         if (args.length <= 0) {
@@ -29,7 +63,7 @@ public class SternalCommand implements CommandExecutor {
             return true;
         }
 
-        switch (args[0].toLowerCase(Locale.ROOT)) {
+        switch (args[0].toLowerCase()) {
             case "toggle":
                 if (!player.hasPermission("sternalboard.toggle")) {
                     player.sendMessage(
