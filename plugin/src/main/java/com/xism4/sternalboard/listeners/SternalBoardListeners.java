57a264c9bf2d8f1a1c7f0837fa2982e2951acc20
xIsm4 <minelatinsoporte@gmail.com>
2022-05-28 13:56:52 +0200

    feat: Register both listeners on same class

---
+++ plugin/src/main/java/com/xism4/sternalboard/listeners/SternalBoardListeners.java
@@ -0,0 +1,26 @@
+package com.xism4.sternalboard.listeners;
+
+import com.xism4.sternalboard.SternalBoard;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class SternalBoardListeners implements Listener {
+
+    private final SternalBoard core;
+
+    public SternalBoardListeners(SternalBoard core) {
+        this.core = core;
+    }
+
+    @EventHandler
+    private void onJoin(PlayerJoinEvent e) {
+        core.getScoreboardManager().setScoreboard(e.getPlayer());
+    }
+
+    @EventHandler
+    private void onQuit(PlayerQuitEvent e) {
+        core.getScoreboardManager().removeScoreboard(e.getPlayer());
+    }
+}
