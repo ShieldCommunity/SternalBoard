529d385afd2c4a8d41e10f95897c90876fae6006
xism4 <soportexism4@gmail.com>
2025-01-22 03:59:56 +0100

    Fix colours parsing

--- plugin/src/main/java/com/xism4/sternalboard/utils/TextUtils.java
+++ plugin/src/main/java/com/xism4/sternalboard/utils/TextUtils.java
@@ -1,11 +1,12 @@
 package com.xism4.sternalboard.utils;
 
-import com.xism4.sternalboard.SternalBoardHandler;
 import com.xism4.sternalboard.SternalBoardPlugin;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.ChatColor;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.regex.Matcher;
@@ -16,41 +17,47 @@ public class TextUtils {
     private static final String MINI_MESSAGE_HEX = "<color:{color}>";
     private static final Pattern HEX_PATTERN = Pattern.compile("(#|&#)([A-Fa-f0-9]){6}");
 
+    private static final int SERVER_VERSION = Integer.parseInt(
+            Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]
+    );
+
     public static String colorize(String text) {
-        if (SternalBoardHandler.VersionType.V1_13.isLowerOrEqual()) {
+
+        if (SERVER_VERSION < 16) {
             return ChatColor.translateAlternateColorCodes('&', text);
         }
 
         text = transformLegacyHex(text);
-        text = text.replace("§", "&");
+        text = text.replaceAll("§", "&");
 
         MiniMessage mm = MiniMessage.miniMessage();
-        return LegacyComponentSerializer.legacySection().serialize(mm.deserialize(text));
+        text = LegacyComponentSerializer.legacySection().serialize(mm.deserialize(text));
+
+        return ChatColor.translateAlternateColorCodes(
+                '&', text
+        );
     }
 
     public static String transformLegacyHex(String text) {
-        StringBuilder result = new StringBuilder();
-        int lastEnd = 0;
-
         Matcher matcher = HEX_PATTERN.matcher(text);
-        while (matcher.find()) {
-            result.append(text, lastEnd, matcher.start());
 
+        while (matcher.find()) {
             String hex = matcher.group()
                     .replace("<", "")
                     .replace(">", "")
                     .replace("&", "");
 
-            result.append(MINI_MESSAGE_HEX.replace("{color}", hex));
-            lastEnd = matcher.end();
+            text = text.replace(matcher.group(), MINI_MESSAGE_HEX.replace("{color}", hex));
         }
 
-        result.append(text.substring(lastEnd));
-        return result.toString();
+        return text;
     }
 
     public static String processPlaceholders(SternalBoardPlugin plugin, Player player, String text) {
-        return plugin.placeholderCheck() ? colorize(PlaceholderAPI.setPlaceholders(player, text)) : colorize(text);
-    }
+        if (plugin.placeholderCheck()) {
+            return colorize(PlaceholderAPI.setPlaceholders(player, text));
+        }
 
+        return colorize(text);
+    }
 }
