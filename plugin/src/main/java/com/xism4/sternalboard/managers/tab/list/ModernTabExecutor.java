6880115e830f555985f856dcbde68e9255dc6f6a
xism4 <soportexism4@gmail.com>
2025-02-12 01:11:09 +0100

    Dont send modern tab for legacy versions

--- plugin/src/main/java/com/xism4/sternalboard/managers/tab/list/ModernTabExecutor.java
+++ plugin/src/main/java/com/xism4/sternalboard/managers/tab/list/ModernTabExecutor.java
@@ -1,8 +1,10 @@
 package com.xism4.sternalboard.managers.tab.list;
 
+import com.xism4.sternalboard.SternalBoardHandler;
 import com.xism4.sternalboard.SternalBoardPlugin;
 import com.xism4.sternalboard.managers.tab.TabExecutor;
 import com.xism4.sternalboard.utils.TextUtils;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 public class ModernTabExecutor extends TabExecutor {
@@ -14,9 +16,11 @@ public class ModernTabExecutor extends TabExecutor {
 
     @Override
     public void sendTab(Player player, String header, String footer) {
-        player.setPlayerListHeaderFooter(
-                TextUtils.processPlaceholders(plugin, player, check(header)),
-                TextUtils.processPlaceholders(plugin, player, check(footer))
-        );
+        if (!SternalBoardHandler.VersionType.V1_17.isHigherOrEqual()) return;
+
+        Component headerComponent = Component.text(TextUtils.processPlaceholders(plugin, player, check(header)));
+        Component footerComponent = Component.text(TextUtils.processPlaceholders(plugin, player, check(footer)));
+
+        player.sendPlayerListHeaderAndFooter(headerComponent, footerComponent);
     }
 }
