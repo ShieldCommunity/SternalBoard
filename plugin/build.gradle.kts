e7d7eaef2e08dfddb1c8edf1abf0f3b79c26b4fe
xism4 <soportexism4@gmail.com>
2025-02-27 22:19:26 +0100

    Implement Zapper

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,43 +6,55 @@ plugins {
 
 repositories {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://repo.alessiodp.com/releases/")
-    maven("https://nexus.neetgames.com/repository/maven-releases/")
     maven("https://jitpack.io/")
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://plugins.gradle.org/m2/")
     maven("https://repo.triumphteam.dev/snapshots/")
     mavenCentral()
 }
 
 dependencies {
     api(project(":api"))
+
     compileOnly(libs.placeholder)
     compileOnly(libs.paper)
-
-    compileOnly(libs.minimessage)
-    compileOnly(libs.gsonAdventure)
-    compileOnly(libs.legacySerializer)
     compileOnly(libs.tinylog)
 
-    implementation(libs.cmd)
-    implementation(libs.inject)
-    implementation(libs.tinylogImpl)
-    implementation("com.alessiodp.libby:libby-bukkit:2.0.0-SNAPSHOT")
+    zap(libs.minimessage)
+    zap(libs.gsonAdventure)
+    zap(libs.legacySerializer)
+    zap(libs.inject)
+    zap(libs.tinylogImpl)
+
+    implementation(libs.cmd) //only provides snapshot build
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
+zapper {
+    libsFolder = "libraries"
+
+    relocationPrefix = "com.xism4.sternalboard.libs"
+
+    repositories {
+        maven("https://jitpack.io/")
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://repo.alessiodp.com/releases/")
+        maven("https://plugins.gradle.org/m2/")
+        includeProjectRepositories()
+    }
+
+    relocate("net.elytrium.serializer", "serializer")
+    relocate("team.unnamed", "injector")
+    relocate("net.kyori", "adventure")
+    relocate("org.tinylog", "tinylog")
+}
+
 tasks {
     shadowJar {
-        relocate("com.alessiodp.libby", "com.xism4.sternalboard.libs")
-        relocate("net.kyori", "com.xism4.sternalboard.libs.kyori")
-        relocate("dev.triumphteam", "com.xism4.sternalboard.libs.commands")
-        relocate("org.tinylog", "com.xism4.sternalboard.libs.logger")
-        relocate("team.unnamed", "com.xism4.sternalboard.libs.injector")
         archiveBaseName.set("SternalBoard")
+        relocate("dev.triumphteam", "com.xism4.sternalboard.libs.commands") //same as up
         destinationDirectory.set(file("$rootDir/bin/"))
         minimize()
     }
--- plugin/src/main/java/com/xism4/sternalboard/SternalBoardPlugin.java
+++ plugin/src/main/java/com/xism4/sternalboard/SternalBoardPlugin.java
@@ -1,18 +1,18 @@
 package com.xism4.sternalboard;
 
-import com.xism4.sternalboard.misc.Bookstores;
 import com.xism4.sternalboard.module.PluginModule;
 import com.xism4.sternalboard.service.Service;
 import com.xism4.sternalboard.misc.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.tinylog.Logger;
+import revxrsal.zapper.ZapperJavaPlugin;
 import team.unnamed.inject.Inject;
 import team.unnamed.inject.Injector;
 
 import java.util.Set;
 
-public class SternalBoardPlugin extends JavaPlugin {
+public class SternalBoardPlugin extends ZapperJavaPlugin {
 
     private static final int STERNAL_ID_METRICS = 13409;
 
@@ -27,7 +27,6 @@ public class SternalBoardPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         Logger.info("Starting SternalBoard Plugin");
-        Bookstores.loadLibs(this);
         this.services.forEach(Service::start);
         Bukkit.getScheduler().runTaskAsynchronously(this, () -> new Metrics(this, STERNAL_ID_METRICS));
     }
