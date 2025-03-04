a3f41389ab2c57b43cd6802bebc0965612a3e129
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2025-03-04 16:37:41 -0300

    refactor(TextUtils): Fix hex color tag format, Parse both legacy and mini message format to versions above 1.16, legacy format will be used if lower than 1.16 until no longer supported.

--- plugin/src/main/java/com/xism4/sternalboard/util/TextUtils.java
+++ plugin/src/main/java/com/xism4/sternalboard/util/TextUtils.java
@@ -1,12 +1,14 @@
 package com.xism4.sternalboard.util;
 
-import com.xism4.sternalboard.SternalBoardPlugin;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Pattern;
 
@@ -15,19 +17,39 @@ public final class TextUtils {
     private static final Pattern HEX_PATTERN = Pattern.compile("(#|&#)([A-Fa-f0-9]{6})");
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
     private static final LegacyComponentSerializer LEGACY_SERIALIZER = LegacyComponentSerializer.legacySection();
+    private static final int SERVER_VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
 
     public static String colorize(String text) {
         if (text == null || text.isEmpty()) return "";
 
+        boolean containsLegacyFormat = text.contains("&") || text.contains("§");
+        if (containsLegacyFormat && SERVER_VERSION < 16) {
+            return ChatColor.translateAlternateColorCodes('&', text);
+        }
+
         text = transformLegacyHex(text);
-
-        if (text.contains("&") || text.contains("§")) return ChatColor.translateAlternateColorCodes('&', text);
-
+        text = containsLegacyFormat ? formatMiniMessage(text) : text;
         return LEGACY_SERIALIZER.serialize(MINI_MESSAGE.deserialize(text));
     }
 
+    public static Component asComponent(String text) {
+        return MINI_MESSAGE.deserialize(formatMiniMessage(text));
+    }
+
+    // Workaround to support both legacy and minimessage format.
+    private static @NotNull String formatMiniMessage(String text) {
+        // Parse legacy color format.
+        text = ChatColor.translateAlternateColorCodes('&', text);
+        // Parse legacy to minimessage format.
+        TextComponent textComponent = LEGACY_SERIALIZER.deserialize(text);
+        // Set component to serialized format.
+        String serialized = MINI_MESSAGE.serialize(textComponent.compact());
+        // Remove backslashes to normalize mini-message format.
+        return serialized.replace("\\", "");
+    }
+
     public static String transformLegacyHex(String text) {
-        return HEX_PATTERN.matcher(text).replaceAll(matcher -> "<color:" + matcher.group(2) + ">");
+        return HEX_PATTERN.matcher(text).replaceAll(matcher -> "<color:#" + matcher.group(2) + ">");
     }
 
     public static String processPlaceholders(final Player player, String text) {
