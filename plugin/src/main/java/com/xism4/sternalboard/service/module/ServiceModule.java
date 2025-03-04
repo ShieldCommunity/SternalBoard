8bb1ba2edf3dbb3c124dc4163a54427c39f6ac75
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2025-03-04 08:41:56 -0300

    chore(Services): Register ManagerService and initialize managers.

--- plugin/src/main/java/com/xism4/sternalboard/service/module/ServiceModule.java
+++ plugin/src/main/java/com/xism4/sternalboard/service/module/ServiceModule.java
@@ -2,6 +2,7 @@ package com.xism4.sternalboard.service.module;
 
 import com.xism4.sternalboard.command.service.CommandService;
 import com.xism4.sternalboard.listener.service.ListenerService;
+import com.xism4.sternalboard.manager.service.ManagerService;
 import com.xism4.sternalboard.service.Service;
 import team.unnamed.inject.AbstractModule;
 
@@ -11,6 +12,7 @@ public class ServiceModule extends AbstractModule {
     protected void configure() {
         super.multibind(Service.class)
                 .asSet()
+                .to(ManagerService.class)
                 .to(CommandService.class)
                 .to(ListenerService.class)
                 .singleton();
